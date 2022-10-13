0fb469f479f17e1e0ee033e0d9af58fc925bc8d8
IgnacioVarisco <varisconacho@gmail.com>
2022-10-13 08:51:57 -0300

    calculadora primer dom

---
+++ main.js
@@ -0,0 +1,16 @@
+const uno = document.getElementById ("uno")
+const dos = document.getElementById ("dos")
+const tres= document.getElementById ("tres")
+const cuatro= document.getElementById ("cuatro")
+const cinco= document.getElementById ("cinco")
+const seis = document.getElementById ("seis")
+const ocho = document.getElementById ("ocho")
+const nueve = document.getElementById ("nueve")
+const cero = document.getElementById ("cero")
+const calculo = document.getElementById("calculo")
+
+
+cero.onclick = () => {calculo.innerText ="cerooooO"}
+
+
+
